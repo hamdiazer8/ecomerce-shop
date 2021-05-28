2beb0777e8b58cb187292e48baf1f77b6b6d3715
AzerHamdi8 <hamdiazer10@gmail.com>
2021-05-28 07:25:28 +0100

    updating the work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React ,{Component} from 'react';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
 
 
 class App extends Component {
