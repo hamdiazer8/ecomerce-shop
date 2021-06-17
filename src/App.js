33f4f296b4e277efba37ee469d0f1c7e2dc9bb58
AzerHamdi8 <hamdiazer10@gmail.com>
2021-06-17 19:31:10 +0100

    fixing firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/signin-and-signup.component';
 import './App.css';
 import Header from './components/header/header.component';
-import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 	
@@ -21,10 +21,8 @@ class App extends Component {
   }
   unsubscribeFromAuth= null;
   componentDidMount(){
-    this.unsubscribeFromAuth= auth.onAuthStateChanged( async user=>{
-      createUserProfileDocument(user)
-      this.setState({currentUser:user}) 
-      console.log(user);
+    this.unsubscribeFromAuth= auth.onAuthStateChanged( async userAuth=>{
+      createUserProfileDocument(userAuth)
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,44 +1,50 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-const config={
-  apiKey: "AIzaSyDa6CJ4nhut2zCQSFeH3OZOCYlIRQZwJA0",
-  authDomain: "clothing-shop-74c78.firebaseapp.com",
-  projectId: "clothing-shop-74c78",
-  storageBucket: "clothing-shop-74c78.appspot.com",
-  messagingSenderId: "575817639472",
-  appId: "1:575817639472:web:39e73c3fe0b02a8201eff5",
-  measurementId: "G-CK34QB38XX"
-};
 
-
-export const createUserProfileDocument = async(userAuth,additionalData)=>{
-
-if(!userAuth) return ;
-const userRef=firestore.doc(`ùsers/${userAuth.uid}`);
-const snapShot=await userRef.get();
-if(!snapShot.exist){
-	const {DisplayName,email}=userAuth;
-	const createdAt=new Date();
-	try{
-		await userRef.set({
-			DisplayName,email,createdAt,...additionalData
-		});
-
-	} catch(err){
-		console.log('error creating user',error.message)
-
-	}
-}
-
-return userRef;
-}
+const config ={
+    apiKey: "AIzaSyDa6CJ4nhut2zCQSFeH3OZOCYlIRQZwJA0",
+    authDomain: "clothing-shop-74c78.firebaseapp.com",
+    projectId: "clothing-shop-74c78",
+    storageBucket: "clothing-shop-74c78.appspot.com",
+    messagingSenderId: "575817639472",
+    appId: "1:575817639472:web:39e73c3fe0b02a8201eff5",
+    measurementId: "G-CK34QB38XX"
+  };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
-export const firestore= firebase.firestore();
-const provider= new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
-export  const signInWithGoogle=()=> auth.signInWithPopup(provider);
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
